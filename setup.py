3099fc67460a429e448d11c6f6bfc851602b7735
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-13 11:26:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, Extension
-import numpy as np
-
-mykmeanssp_module = Extension(
-    'mykmeanssp',
-    sources=['spkmeansmodule.c', 'spkmeans.c'],
-    include_dirs=[np.get_include()],
-)
 
 setup(
     name='mykmeanssp',
-    description='spkmeansmodule.c Module',
-    ext_modules=[mykmeanssp_module]
+    version='2.0.0',
+    author="Almog Altman, Leah London Arazi",
+    description="Preforms the K-means clustering algorithm.",
+    ext_modules=[
+        Extension(
+            # the qualified name of the extension module to build
+            'mykmeanssp',
+            # the files to compile into our module relative to ``setup.py``
+            ['spkmeans.c','spkmeansmodule.c'],
+        ),
+    ]
 )
