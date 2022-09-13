4384bb53d58dcb7b7e1b9f9517f1cd6640af2326
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-13 11:44:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-from setuptools import Extension, setup
+from setuptools import setup , find_packages , Extension
 
-module = Extension("myspkmeans",
-                   sources=[
-                       'spkmeans.c',
-                       'spkmeansmodule.c'
-                   ])
-setup(name='myspkmeans',
-      version='1.0',
-      description='Python wrapper for custom C extension',
-      ext_modules=[module])
+setup(
+    name='spkmeansmodule' ,
+    version='0.1.0' ,
+    author= "eyal&matan" ,
+    
+    author_email="eyalgrinberg@mail.tau.ac.il" ,
+    description= "kmeans C - api" ,
+    install_requires= ['invoke'] , 
+    packages= find_packages(where = '.', exclude=()) ,
+    license= 'GPL-2' , 
+    classifiers= [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)' , 
+        'Natural Language :: English' ,
+        'Programming Language :: Python :: 3 :: Only' , 
+    ] ,
+    ext_modules= [
+        Extension(
+            'spkmeansmodule' ,
+            ['spkmeans.c' , 'spkmeansmodule.c'] ,
+        ) ,
+    ]
+)
