cbcf420bf241ba1f2f17c23820375c614dbd9941
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-13 13:53:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, Extension
 
-setup(name='Project',
-      version='1.0',
-      description='spkmeans algorithm',
-      ext_modules=[Extension('spkmeans_c', sources=['spkmeansmodule.c', 'spkmeans.c']),
-                   Extension('kmeans_c', sources=['kmeansmodule.c','kmeans.c'])])
+module = Extension(
+            "my_spkmeans",
+            sources=["spkmeans.c", "kmeans.c", "spkmeansmodule.c"])
+
+setup(name='my_spkmeans',
+    version='1.0',
+    description='spkmeans C implementation',
+    ext_modules=[module]
+)
