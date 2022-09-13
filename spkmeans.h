c0793388256440cdf77d761212a204158c028306
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-13 13:41:48 +0300

    Update spkmeans.h

--- spkmeans.h
+++ spkmeans.h
@@ -1,28 +1,15 @@
-#ifndef SPKMEANS_H_
-#define SPKMEANS_H_
+#ifndef KMEANS_H_
+#define KMEANS_H_
 
-void wam(double** A, double** df, int N, int d);
-void ddg(double** B, double** A, int N);
-void lnorm(double** W, double** D, int N);
-void jacobi(double** A, double** V, int N);
-void input_error();
-void print_elem(double e, int i, int N);
-void free_matrix(double **matrix, int N);
-void transpose(double** A, int N);
-void matrix_mul(double **A, double **B, double **D, int N);
-void set_IJ(double **A, int *IJ, int N);
-void set_P(double **P, double c, double s, int I, int J, int N);
-void ID(double **I, int N);
-void d_swap(double **A, int i, int j);
-void swap(double **A, int i, int j);
-void sort_matrices(double **A, double **B, int l, int r);
-void exe_goal(double **df_c, double **A, double **B, int N, int d, char *goal);
-void print_resault(double **A, double **B, double **df_c, int N, char *goal);
-double** malloc_matrix(int N, int K);
-double off(double **A, int N);
-double euclidean_norm_sub(double *X, double *Y, int d);
-int min_dist(double *vector, double **centroids, int k, int d);
-int eigengap_heuristic(double **E, int N);
-int main();
+
+/* declaring variables */
+int k, max_iter, iterates, *to_avg, points_counter, cluster_length, assign_to_cluster;
+int i, j, m, p, t, is_norm;
+double *centroids, *datapoints, *cluster_points, temp_point, min_dist, distance, normDelta, eps;
+
+/* declaring functions */
+
+void freePointers();
+double* Kmeans(int k, int n, int d, int max_iter, double epss, double* dp, double *c);
 
 #endif
