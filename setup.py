ac9bfd0fcc380bc69a8c26bfc8d84c7b51d562f7
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-13 13:40:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, Extension
 
-setup(name='myspkmeanssp',
+setup(name='Project',
       version='1.0',
-      description='myspkmeanssp setup.py',
-      ext_modules=[Extension("myspkmeanssp", sources=['spkmeansmodule.c','spkmeans.c'])])
+      description='spkmeans algorithm',
+      ext_modules=[Extension('spkmeans_c', sources=['spkmeansmodule.c', 'spkmeans.c']),
+                   Extension('kmeans_c', sources=['kmeansmodule.c','kmeans.c'])])
