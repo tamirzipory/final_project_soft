bdbf4afe8d85e65a8f4f58b002f98ee3cacef263
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-13 11:28:44 +0300

    Update spkmeans.py

--- spkmeans.py
+++ spkmeans.py
@@ -1,215 +1,220 @@
 import sys
-import numpy as np
+from enum import Enum
 import pandas as pd
-import mykmeanssp
+import numpy as np
+from mykmeanssp import *
 
-# --------------- Auxiliary functions --------------- #
+# kmeans++ random seed
+np.random.seed(0)
 
-def euclid(x, y):  # actually euc^2
-    sumUp = 0
-    dim = len(x)
-    coordinateSub = [0 for init in range(dim)]
-    for coordinate in range(dim):
-        coordinateSub[coordinate] = x[coordinate] - y[coordinate]
-        coordinateSub[coordinate] = coordinateSub[coordinate] * coordinateSub[coordinate]
-        sumUp += coordinateSub[coordinate]
-    res = (sumUp)
+# goal
+class Goal(Enum):
+    SPK = "spk"
+    WAM = "wam"
+    DDG = "ddg"
+    LNORM = "lnorm"
+    JACOBI = "jacobi"
 
-    return res
+# constants
+MAX_ITER = 300
+EPSILON = 0
 
+def check_k(k):
+    """
+    checks if the parameter k is valid and converts it to the right type.
+    :param k
+    :return: the value of the parameter, None in case of an error
+    """
+    if not k.isnumeric(): # checks if the string contains only digits
+        return None
+    k = int(k) # k==0 is valid
+    return k
 
-def getRandCentroids(points, k, indexToKey):
+def check_goal(goal):
+    """
+    checks if the parameter goal is valid and converts it to the right type.
+    :param goal
+    :return: the value of the parameter, None in case of an error
+    """
+    try:
+        goal = Goal(goal)
+    except:
+        return None
+    return goal
 
-    # what we need:
+def get_parameters():
+    """
+    gets the parameters from cmd and checks if they are valid.
+    :return: tuple containing the parameters.
+    """
+    num_params = len(sys.argv) - 1 # without program name
+    k = 0
+    eps = EPSILON
+    max_iter = MAX_ITER
+    input_file_name = ""
+    goal = None
 
-    n = len(points)
-    dim = len(points[1])
+    # get parameters from cmd
+    if num_params != 3:
+        invalid_input()
+    else:
+        k = sys.argv[1]
+        goal = sys.argv[2]
+        input_file_name = sys.argv[3]
 
-    BoolPoints = []
-    for i in range(0, n):
-        BoolPoints.append(False)  # initialize to false values
+    # convert parameters to the right types and check validity
+    k = check_k(k)
+    goal = check_goal(goal)
+    parameters = (k, max_iter, eps, goal, input_file_name)
+    if None in parameters: # at least one of the parameters is not valid
+        invalid_input()
+    return parameters
 
-    Yindexes = []
-    for i in range(0, k):
-        Yindexes.append(-1)  # initialize to -1 values
+def read_input_file(input_file_name):
+    """
+    gets the input file and returns the data points as a pandas Data Frame.
+    the index of the data points is from 0 to (n-1).
+    :param input_file_name: string that represents the input file name.
+    :return: data_points_df, a pandas Data Frame representing the data points.
+    """
+    data_points_df = pd.read_csv(input_file_name, header=None) # will not fail, beacuse file is assumes to be in right foramt
+    return data_points_df
 
-    Dpoints = []
-    for i in range(0, n):
-        Dpoints.append(-1)  # initialize to -1 values
+def kmeans_pp_centroids(data_points_df, k):
+    """
+    this function chooses the random initial centroids from the data points according to the given k.
+    :param data_points_df: the output of "read_input_file"
+    :param k: the desired number of clusters
+    :return: a tuple containing the initial centroids and the initial centroids indexes.
+    """
+    initial_centroids = []
+    initial_centroids_indexes = []
+    # choose first centroid
+    mu_index = np.random.choice(data_points_df.index)
+    mu = list(data_points_df.iloc[mu_index])
+    initial_centroids.append(mu)
+    initial_centroids_indexes.append(mu_index)
+    # choose the rest of the centroids
+    for i in range(1, k):
+        # calculate Dls
+        Dl_results = data_points_df.apply(lambda data_point: min_euclidean_norm_squared(data_point, initial_centroids), axis=1).to_numpy()  # axis 1 - apply on rows
+        # calculate probabilities
+        P_results = Dl_results / np.sum(Dl_results) # sum can not be 0: k > 1, N > 1
+        # choose next centroid
+        mu_index = np.random.choice(data_points_df.index, p=P_results)
+        mu = list(data_points_df.iloc[mu_index])
+        initial_centroids.append(mu)
+        initial_centroids_indexes.append(mu_index)
 
-    Ppoints = []
-    for i in range(0, n):
-        Ppoints.append(0)  # initialize to 0 values
-    toprint = ""
+    return initial_centroids, initial_centroids_indexes
 
-    # actual function:
+def euclidean_norm_squared(vec1, vec2):
+    """
+    gets two vectors (meaning points) and calculates their euclidean norm. returns the squared result.
+    :param vec1: point 1.
+    :param vec2: point 2.
+    :return: (euclidean norm of point 1 and point 2)^2
+    """
+    return sum([(vec1[i] - vec2[i])**2 for i in range(len(vec1))])
 
-    SumD = 0
-    np.random.seed(0)
+def min_euclidean_norm_squared(vec, vec_list):
+    """
+    returns the minimal value of euclidean_norm_squared(vec,x) from all vectors x in vec_list.
+    :param vec: some vector.
+    :param vec_list: list of vectors of the same dimensions.
+    :return: min euclidean_norm_squared.
+    """
+    return min([euclidean_norm_squared(vec, x) for x in vec_list])
 
-    for z in range(0, k):
-        SelectedIndex = 0
-        if (z == 0):
-            SelectedIndex = np.random.choice(n, 1)[0]  # select a random number Rand from 0 to n-1
-            toprint = toprint + str(indexToKey[SelectedIndex])
-            Yindexes[z] = SelectedIndex
-            BoolPoints[SelectedIndex] = True
+def print_matrix(matrix, format_zero = False):
+    """
+    gets a matrix (meaning, a 2 dimensional list) and prints it in the desired format.
+    :param matrix: a 2 dimensional floats list.
+    :param format_zero: boolean flag, removes the "-" from "-0.0000" if on.
+    :return: None.
+    """
+    for row in matrix:
+        for i in range(len(row)):
+            if i < len(row) - 1:
+                out_str = "%.4f," % (row[i])
+            else: # last coordinate in row without comma, and with new line
+                out_str = "%.4f\n" % (row[i])
+            if format_zero:  # if the format_zero flag is on, remove the "-" from zeros
+                out_str = out_str.replace("-0.0000", "0.0000")
+            print(out_str, end="")
 
-        else:  # z!=0
-            SelectedIndex = np.random.choice(n, 1, p=Ppoints)[0]
-            toprint = toprint + "," + str(indexToKey[SelectedIndex])
-            Yindexes[z] = SelectedIndex
-            BoolPoints[SelectedIndex] = True
+def print_final_centroids(initial_centroids_indexes, final_centroids):
+    """
+    gets the final centroids from the kmeans algorithm output and the initial chosen centroids.
+    prints the data in the desired format.
+    :param initial_centroids_indexes: the chosen centroids indexes from kmeans_pp_centroids.
+    :param final_centroids: the final centroids after calling the c module.
+    :return: None.
+    """
+    # print initial centroid indexes
+    print(",".join([str(x) for x in initial_centroids_indexes]))
+    # print final centroids
+    print_matrix(final_centroids)
 
-            if (z == k - 1):  # no need to update Ppoints again
-                break
-        for i in range(0, n):  # reset Dpoints, Ppoints
-            Dpoints[i] = -1
-            Ppoints[i] = 0  # seems like shouldnt reset P
-        SumD = 0
+# error messages
+def invalid_input():
+    print("Invalid Input!", end="")
+    sys.exit()
 
-        # Yindexes[z] = SelectedIndex
-        # BoolPoints[SelectedIndex] = True
+def error():
+    print("An Error Has Occurred", end="")
+    sys.exit()
 
+# main
+def main():
+    k, max_iter, eps, goal, input_file_name = get_parameters()
+    data_points_df = read_input_file(input_file_name)
+    N = data_points_df.shape[0]
+    d = data_points_df.shape[1]
+    data_points = data_points_df.values.tolist()  # convert data points to 2D list
 
-        for i in range(0, n):
-            for j in range(0, z + 1):
-                Yj = points[Yindexes[j]]
-                CurrDi = euclid(points[i], Yj)
-                # CurrDi = CurrDi * CurrDi
-                if (Dpoints[i] == -1 or CurrDi < Dpoints[i]):
-                    Dpoints[i] = CurrDi
-        SumD = sum(Dpoints)
+    # kmeans option
+    if goal == Goal.SPK:
+        if k == 1 or k >= N: # check k for SPK option
+            invalid_input()
+        try:
+            t_matrix = np.array(fit_nsc(data_points, N, d, k)) # calculate t matrix and find k if k=0
+        except:
+            error()
+        # change the k and d according to the t matrix that was calculated
+        k = t_matrix.shape[1]
+        d = k
+        # the data points are the rows of t matrix
+        data_points_df = pd.DataFrame(t_matrix)
+        data_points = data_points_df.values.tolist()
 
-        for index in range(0, n):
-            Ppoints[index] = Dpoints[index] / SumD
+        # find the initial centroids for the kmeans algorithm
+        initial_centroids, initial_centroids_indexes = kmeans_pp_centroids(data_points_df, k)
+        try:
+            final_centroids = np.array(fit_kmeanspp(data_points, initial_centroids, N, d, k, max_iter, eps))
+        except:
+            error()
+        print_final_centroids(initial_centroids_indexes, final_centroids)
+            
+    # jacobi option
+    elif goal == Goal.JACOBI:
+        if N != d: # check that data_points is a square matrix
+            invalid_input()
+        try:
+            out = calculate_jacobi(data_points, N, d) # in this case data_points is assumed to be a symmetric matrix
+        except:
+            error()
+        print_matrix(out[0], format_zero=True) # prints the eigenvalues without the "-" before zeros
+        print_matrix(out[1])
 
-    CentArray = []
-    for i in range(0, k):
-        CentArray.append(points[Yindexes[i]])
+    # matrix options
+    # goal in [Goal.WAM, goal.DDG, goal.LNORM]
+    else:
+        try:
+            out = np.array(calculate_matrix(data_points, N, d, goal.value))
+        except:
+            error()
+        print_matrix(out)
 
-    # print("toprint:", toprint)
-    return CentArray, Yindexes
-
-
-# print the recieved matrix
-def printResult(ArrayToPrint, rowDim, colDim):
-    ArrayToPrint = np.round(ArrayToPrint, decimals=4)
-    for i in range(rowDim):
-        lineString = ""
-        for j in range(colDim):
-            if j > 0:
-                lineString += ","
-            if abs(ArrayToPrint[i * colDim + j]) < 0.00005:
-                lineString += "%.4f" % abs(ArrayToPrint[i * colDim + j])
-            else:
-                #lineString += str(ArrayToPrint[i * colDim + j])
-                lineString += "%.4f" % (ArrayToPrint[i * colDim + j])
-        print(lineString.replace("[", "").replace("]", ""))
-
-
-# --------------- K-means-PP algorithm --------------- #
-
-def kmeansPP(points, dim, n, k):
-
-    # ----- verify length's correctness ----- #
-
-    # K must be bigger than zero and integer
-    if k < 1 or type(k) != int:
-        sys.exit("An Error Has Occured")
-    if n == 0:  # At least one point is needed
-        sys.exit("An Error Has Occured")
-    if n <= k:  # K must be smaller than N and not equal to N
-        sys.exit("An Error Has Occured")
-    if dim == 0:  # Dim of points is zero
-        sys.exit("An Error Has Occured")
-
-    # ----- indexing centroids  ----- #
-    
-    points = pd.DataFrame(points)
-    
-    indexToKey = []  # indexToKey[i] is the i-th smallest key.
-    for i in range(len(points.index)):
-        indexToKey.append(int(points.index[i]))
-
-    # ----- choosing random initial centroids  ----- #
-
-    regPoints = np.array(points).tolist()
-    _, centroidsIndexes = getRandCentroids(regPoints, k, indexToKey)
-
-    # ----- print random initial centroids  ----- #
-
-    print(",".join([str(index) for index in centroidsIndexes]))
-
-    # ----- create arrays for kmeans algo in c  ----- #
-
-    centroids_vals = []
-    for cent_index in centroidsIndexes:
-        centroids_vals.append(points.loc[cent_index])
-    centroids = pd.DataFrame(centroids_vals)
-
-    centroidsResult = np.zeros(dim * k)
-
-    # ----- calls kmeans in c through fit function in API  ----- #
-    # maxIter = 300
-
-    mykmeanssp.fit(points, centroidsResult, centroids, dim, k, 300)
-
-    # ----- printing results ----- #
-    printResult(centroidsResult, k, dim)
-
-
-# --------------- main  --------------- #
-
-if len(sys.argv) != 4:
-    sys.exit("An Error Has Occured")
-
-# ----- calling SPKmeans in c ----- #
-kStr = sys.argv[1]
-goalStr = sys.argv[2]
-fileNameStr = sys.argv[3]
-
-n = 0
-with open(fileNameStr, 'r') as fin:
-  for line in fin:
-    n += 1
-
-# TODO: get n,k from file!!!
-#n = 10
-k = int(kStr)
-x,y = n+1, n
-#results = np.zeros((n+1, n))
-results = np.zeros((x, y))
-k_ans = np.zeros((1, 1))
-
-mykmeanssp.fitToSPKmeans(kStr, goalStr, fileNameStr, results, k_ans)
-
-# ----- taking k as int ----- #
-k = int(kStr)
-
-if goalStr == "spk":
-  k_ans = int(k_ans[0][0])
-  results.shape = (x*y, )
-  results = results[0:n*k_ans]
-  results.shape = (n, k_ans)
-  kmeansPP(results, k_ans, n, k_ans)
-
-elif goalStr == "jacobi":   # (n+1) * n
-  results.shape = (x*y, 1)
-  results = results[0:(n+1)*n]
-  printResult(results, n+1, n)
-
-elif goalStr == "wam":
-  results.shape = (x*y, )
-  results = results[0:n*n]
-  printResult(results, n, n)
-
-elif goalStr == "ddg":
-  results.shape = (x*y, )
-  results = results[0:n*n]
-  printResult(results, n, n)
-
-elif goalStr == "lnorm":
-  results.shape = (x*y, )
-  results = results[0:n*n]
-  printResult(results, n, n)
+if __name__ == "__main__":
+    main()
