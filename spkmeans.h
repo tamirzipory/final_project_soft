902473f67b7ffd1fce08e6ac72af10cca51bd49d
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-13 11:34:21 +0300

    Update spkmeans.h

--- spkmeans.h
+++ spkmeans.h
@@ -1,106 +1,52 @@
 #ifndef SPKMEANS_H_
 #define SPKMEANS_H_
 
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
+void readfile();
+void init_cendroids(int clusters_num);
+double distance(double *v1, double *v2);
+int min_dist_centroid(double *v);
+void vector_to_cluster(int clusters_num);
+double *cluster_to_centroid(int index);
+int areequal(double *arr1, double *arr2);
+int update_centroids();
+double **calccentroids(int max_iter);
+double **matMult(double **A, double **B);
+double **weightedAdjMat();
+double weightedAdjMat_calc(double *v1, double *v2);
+double **Lnorm();
+void printMat(double **Mat, int rowNum, int colNum);
+double **diagDegMat();
+void calcRotationMat(double **P, double c, double s, int row, int column);
+double calc_theta(double **Mat, int i, int j);
+double calc_t(double theta);
+double calc_c(double t);
+double calc_s(double t, double c);
+int isConverged(double **A, double **Aprime);
+void calcAprime(double **A, double **Aprime, int i, int j, double c, double s);
+double **calcJacobi(double **A);
+int eigenComperator(const void *a, const void *b);
+void sortEigenVectors();
+int eigengapHeuristic();
+void createTMat();
+void fullSpectral();
+double **transpMat(double **Mat);
+void assertmsg(int x);
+void freearray(double **array, int length);
 
-/*constants*/
-#define CONVERGENCE_EPSILON pow(10, -5)
-#define MAX_ITER_JACOBI 100
-#define NUMBER_OF_VALID_ENUM_OPTIONS 4
-
-/*structs*/
-/*struct that represents a matrix of doubles, including it's dimensions*/
-typedef struct
-{
-    double **values;
-    int rows;
-    int columns;
-} matrix;
-
-/*struct for the output of the jacobi algorithm.
-the struct will conatin:
-1. eigenvalues - the diagonal of A', represented as one dimeantional matrix (1 row).
-2. eigenvectors - V matrix.*/
-typedef struct
-{
-    matrix *eigenvalues;
-    matrix *eigenvectors_mat;
-} eigen_data;
-
-/*struct for connection between eigenvalue to it's eigenvector (which is represented as one dimeantional matrix (1 row))*/
-typedef struct
-{
-    matrix *eigenvector;
-    double eigenvalue;
-} eigen_pair;
-
-/*strcut cluster, representing a cluster of the kmeans++ algorithm.*/
-typedef struct
-{
-    int *points_indexes;
-    int num_points;
-} cluster;
-
-/*enum representing the output options*/
-typedef enum
-{
-    wam,
-    ddg,
-    lnorm,
-    jacobi,
-    invalid
-} GOAL;
-
-/*for conversion function, from https://stackoverflow.com/questions/16844728/converting-from-string-to-enum-in-c*/
-static const struct
-{
-    GOAL val;
-    const char *str;
-} conversion[] = {{wam, "wam"}, {ddg, "ddg"}, {lnorm, "lnorm"}, {jacobi, "jacobi"}};
-
-/*declaretion - general*/
-GOAL string_to_goal(const char *str);
-
-/*declarations - The Normalized Spectral Clustering Algorithm*/
-double euclidean_norm_squared(double *point1, double *point2, int d);
-matrix *create_matrix(int rows, int columns);
-void print_matrix(matrix *mat);
-void print_matrix_formatted(matrix *mat, int format_zero);
-matrix *dot_product(matrix *mat1, matrix *mat2);
-matrix *calculate_w(matrix *data_points);
-matrix *calculate_d_from_w(matrix *w_mat);
-matrix *calculate_d(matrix *data_points);
-void sqrt_diagonal(matrix *mat);
-matrix *calculate_l(matrix *data_points);
-void normalize_matrix(matrix *mat);
-matrix *create_identity_matrix(int n);
-eigen_data *calculate_jacobi(matrix *a);
-matrix *calculate_p(matrix *a, int i, int j);
-void find_pivot(matrix *a, int *i, int *j);
-void rotate_matrix(matrix *a, matrix *p, int i, int j);
-double off(matrix *a);
-int compare_eigen_pair(const void *p1, const void *p2);
-eigen_pair *extract_sorted_eigen_pairs(eigen_data *jacobi_data);
-int find_k_heuristic(eigen_pair *eigen_pairs, int n);
-matrix *calculate_t(eigen_pair *eigen_pairs, int n, int k);
-matrix *fit_nsc_c(matrix *data_points, int k);
-void free_matrix(matrix *mat);
-void free_eigen_data(eigen_data *jacobi_data);
-void free_eigen_pairs(eigen_pair *eigen_pairs, int length);
-
-/*declarations - kmeans++ algorithm*/
-matrix *read_input_file(char *input_file_name);
-matrix *fit_kmeanspp_c(matrix *data_points, matrix *centroids, int max_iter, double epsilon);
-void reset_clusters(cluster *clusters, int k);
-cluster *create_clusters(int k, int n);
-int find_closest_centroid(double *point, matrix *centroids);
-void add_point(cluster *cluster, int point_index);
-int update_centroid(cluster *current_cluster, double *current_centroid, double *new_centroid, matrix *data_points, double epsilon);
-void free_clusters(cluster *clusters, int k);
-int invalid_input();
-int error();
+double **jacobi(double **, int);
+double *get_diag(double **, int);
+void print_row(double *, int);
+double *get_ith_column(double **, int, int);
+void free_mat(double **);
+double **gen_id_mat(int);
+double off(double **, int);
+void A_to_A_tag(double **, double **, int);
+void assert_double_arr(const double *arr);
+void assert_double_mat(double **mat);
+int *max_indices_off_diag(double **, int);
+int sign(double);
+void V_multi_P(double **, double, double, int, int, int);
+void print_double(double);
+void assert_int_arr(const int *arr);
 
 #endif
