118314d32a1b8f6e7237c2a272ea4ddd5f20529d
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-13 13:51:52 +0300

    Update spkmeansmodule.c

--- spkmeansmodule.c
+++ spkmeansmodule.c
@@ -1,66 +1,161 @@
-#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include "spkmeans.h"
 
-
-static PyObject *spkmeans(PyObject *self, PyObject *args)
+/* Receives N, K, D, Datapoints/matrix, goal, Centroids from python
+ * Returns correspond matrix according to given goal*/
+static PyObject *fit(PyObject *self, PyObject *args)
 {
-    /* Terminates invalid parameters from spkmeans.py */
-    if (PyArg_ParseTuple(args, "isii:spkmeans", &goal_int, &file_name, &n, &d)) {
-        /*
-        if (goal_int==0){
-             This builds the answer ("d" = Convert a C double to a Python floating point number) back into a python object 
-            return Py_BuildValue("i", Spkmeans(file_name, n, d));   Py_BuildValue(...) returns a PyObject*  
-        } else {
-        */
-            Matrix M =  MatOutput(goal_int, file_name, n, d);
-            k = M.M;
-            PyObject* nodes = PyList_New(M.N);
-            for (i = 0; i < M.N; ++i){
-                PyObject* node = Py_BuildValue("d", M.nodes[i]);
-                PyList_SetItem(nodes, i, node);
-            }
+    PyObject *Datapoints_PyObject;
+    PyObject *Centroids_PyObject;
+    PyObject *current_datapoint;
+    PyObject *current_double;
+    PyObject *returned_result;
+    PyObject *current_vector;
+    PyObject *current_centroid;
 
-            PyObject* edges = PyList_New(M.N);
-            for (i = 0; i < M.N; ++i){
-                PyObject* row = PyList_New(M.M);
-                for (j = 0; j < M.M; ++j){
-                    PyObject* edge = Py_BuildValue("d", M.edges[i][j]);
-                    PyList_SetItem(row, j, edge);
-                }
-                PyList_SetItem(edges, i, row);
-            }
+    int N, K, D, i, j, rows, cols, cols_allocation, return_value;
+    double **Datapoints, **Centroids, **goal_result;
+    enum Goal goal;
+    
+    /* args= N, K, D, Datapoints/matrix, goal, Centroids*/
+    /* (IF: Goal= wam, ddg, lnorm, jacobi, spk(1)): args= N, K, D, Datapoints/matrix, goal */
+    /* (IF: Goal= spk(2)): args= N, K, D, Datapoints/matrix, goal, Centroids*/
+    /* Receiving args from Python program*/
+    if (!PyArg_ParseTuple(args, "iiiOiO", &N, &K, &D, &Datapoints_PyObject, &goal, &Centroids_PyObject))
+    {
+        PyErr_SetString(PyExc_RuntimeError, ERROR);
+        return NULL;
+    }
 
-            free_matrix(M);
-            return Py_BuildValue("{s:O,s:O,s:i}", "nodes", nodes, "edges", edges, "k", k);
+    cols_allocation=D;
+    /* Only in spk-ex2 (when we return to fit in the second time), Datapoints need to have D+1 cols*/
+    if(goal==SPK_EX2)
+        cols_allocation=D+1;
+
+    /* Set up Datapoints and Centroids's matrix*/
+    Datapoints = matrix_allocation(N, cols_allocation);
+    if (Datapoints == NULL)
+    {
+        PyErr_SetString(PyExc_RuntimeError, ERROR);
+        return NULL;
+    }
+
+    current_centroid=NULL;
+    Centroids=NULL;
+    /* Only in spk-ex2: sets Centroids*/
+    if(goal == SPK_EX2)
+    {
+        Centroids = matrix_allocation(K, cols_allocation);
+        if (Centroids == NULL)
+        {
+            free_memory(Datapoints, N);
+            PyErr_SetString(PyExc_RuntimeError, ERROR);
+            return NULL;
+        }
     }
     
-   
-    
-    return NULL;
+    /* Fill matrix values as given list from python*/
+    for (i = 0; i < N; i++)
+    {
+        current_datapoint = PyList_GetItem(Datapoints_PyObject, i);
+        if (i < K && goal == SPK_EX2)
+            current_centroid = PyList_GetItem(Centroids_PyObject, i);
+
+        /*Set up each of vector*/
+        for (j = 0; j < D; j++)
+        {
+            current_double = PyList_GetItem(current_datapoint, j);
+            Datapoints[i][j] = PyFloat_AsDouble(current_double);
+            if (i < K && goal == SPK_EX2)
+            {
+                current_double = PyList_GetItem(current_centroid, j);
+                Centroids[i][j] = PyFloat_AsDouble(current_double);
+            }
+        }
+
+        /* Only in spk-ex2: Zero in last cell [dimension]*/
+        if(goal == SPK_EX2)
+        {
+            Datapoints[i][j] = 0;
+            if (i < K)
+            {
+                Centroids[i][j] = 0;
+            }
+        }
+    }
+
+    /* If goal is spk_ex2 (spk second run) run kMeans from ex2 else- goal is wam, ddg, lnorm or spk (first run)- use run_goal function*/
+    if (goal == SPK_EX2)
+    {
+        return_value = kMeans(N, K, Datapoints, Centroids, D);
+        if (return_value == FAIL)
+        {
+            free_memory(Datapoints,N);
+            free_memory(Centroids,K);
+            PyErr_SetString(PyExc_RuntimeError, ERROR);
+            return NULL;
+        }
+        goal_result = Centroids;
+        rows = K; /*rows=number of clusters/centroids=K*/
+        cols=D; /*cols= dimension*/
+    }
+    else
+    {
+        goal_result = run_goal(goal, Datapoints, N, D, &K);
+        if (goal_result == NULL)
+        {
+            free_memory(Datapoints, N);
+            PyErr_SetString(PyExc_RuntimeError, ERROR);
+            return NULL;
+        }
+        rows = (goal == JACOBI) ? (N + 1) : N; /*Jacobi needs N+1 rows*/
+        cols=N; /* In wam,ddg,lnorm,jacobi*/
+        if(goal==SPK)
+            cols=K; /* In spk (first run)- T's dimesnions are N*K (updated/original k) */
+    }
+
+    /* Converts result_matrix to an array list (python)*/
+    returned_result = PyList_New(rows);
+    for (i = 0; i < rows; ++i)
+    {
+        current_vector = PyList_New(cols);
+        for (j = 0; j < cols; j++)
+        {
+            PyList_SetItem(current_vector, j, Py_BuildValue("d", goal_result[i][j]));
+        }
+        PyList_SetItem(returned_result, i, Py_BuildValue("O", current_vector));
+    }
+
+    free_memory(Datapoints, N);
+    free_memory(goal_result, rows);
+
+    return returned_result;
 }
 
-
-static PyMethodDef capiMethod[] = {
-    {"fit",                                                    /* the Python method name that will be used */
-     (PyCFunction)spkmeans,                                      /* the C-function that implements the Python function and returns static PyObject*  */
-     METH_VARARGS,                                             /* flags indicating parameters accepted for this function */
-     PyDoc_STR("spkmeans algorithm for given data of vectors")}, /*  The docstring for the function */
-    {NULL, NULL, 0, NULL}                                      /* The last entry must be all NULL as shown to act as a
-                                 sentinel. Python looks for this entry to know that all
-                                 of the functions for the module have been defined. */
+/* ============== C Python API ============== */
+static PyMethodDef Methods[] = {
+        {"fit",
+                (PyCFunction)fit,
+                     METH_VARARGS,
+                        NULL},
+        {NULL, NULL, 0, NULL}
 };
 
-static struct PyModuleDef moduledef = {
-    PyModuleDef_HEAD_INIT,
-    "spkmeans_c", /* name of module */
-    NULL,       /* module documentation, may be NULL */
-    -1,         /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
-    capiMethod  /* the PyMethodDef array from before containing the methods of the extension */
+static struct PyModuleDef moudledef = {
+        PyModuleDef_HEAD_INIT,
+        "my_spkmeans",
+        NULL,
+        -1,
+        Methods
 };
 
-PyMODINIT_FUNC
-PyInit_spkmeans_c(void)
+PyMODINIT_FUNC PyInit_my_spkmeans(void)
 {
-    return PyModule_Create(&moduledef);
+    PyObject *m;
+    m = PyModule_Create(&moudledef);
+    if (!m)
+    {
+        return NULL;
+    }
+    return m;
 }
