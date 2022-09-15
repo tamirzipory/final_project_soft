b6da78b955c220a9a924bcbf26e838c02151cb7a
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-15 17:46:27 +0300

    Update spkmeansmodule.c

--- spkmeansmodule.c
+++ spkmeansmodule.c
@@ -21,7 +21,7 @@ static PyObject *fit(PyObject *self, PyObject *args)
     if(goal==6)
         num_of_vectors_allocation=D+1;
 
-    Datapoints = matrix_allocation(len, num_of_vectors_allocation);
+    Datapoints = alloc_for_mat(len, num_of_vectors_allocation);
     if (Datapoints == NULL){
         /*I dont know if we only need to return NULL, but my friends that we help them say that it's reccomend*/
         PyErr_SetString(PyExc_RuntimeError, "An Error Has Occurred");
@@ -31,7 +31,7 @@ static PyObject *fit(PyObject *self, PyObject *args)
     
    
     if(goal == 6){
-        Centroids = matrix_allocation(K, num_of_vectors_allocation);
+        Centroids = alloc_for_mat(K, num_of_vectors_allocation);
         if (Centroids == NULL){
             free_memory(Datapoints, len);
             PyErr_SetString(PyExc_RuntimeError, "An Error Has Occurred");
