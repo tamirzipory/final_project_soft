b4cf111a1980694a90d000575f14a8c7a2b11a0a
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-15 16:40:50 +0300

    Update spk.c

--- spk.c
+++ spk.c
@@ -1,25 +1,60 @@
-double **cal_spk(double **lnorm, int len, int *K){ 
-    double **jacobi_pattern, **eigenvectors, **the_T_mat, **miun_T_mat;
+double **set_T(double **the_u_mat, int len, int K){
+    int i, j, in;
+    double sum = 0;
+    double **the_T_mat = alloc_for_mat(len, K);
+    if (NULL == the_T_mat)
+         return NULL;
+    i = 0;
+    while (i < len){
+        j = 0;
+        while (j < K){
+             if (j == 0){
+                sum = 0;
+                for (in = 0; in < K; in++)
+                    sum = sum + (the_u_mat[i][in] * the_u_mat[i][in]);
+            }
+            if(sum != 0)
+                the_T_mat[i][j] = the_u_mat[i][j] / sqrt(sum);
+            else the_T_mat[i][j] = 0;
+        j++;
+        }
+        i++;
+    }
+    return the_T_mat;
+}
 
-    jacobi_pattern = (double **)calc_jacob(len, lnorm);
-    if (jacobi_pattern == NULL)
+void heru_eigen(double *values, int len, int *K){ 
+    int i;
+    double curr_max_gap = DBL_MIN;
+    i =0;
+    while((int)(len / 2) >= i){
+         if (fabs(values[i-1]-values[i]) > curr_max_gap){
+            curr_max_gap = fabs(values[i-1]-values[i]);
+            *K = i;
+        }
+        i++;
+    }
+}
+double **cal_spk(double **lNormat, int len, int *K){ 
+    double **patt_jacobi;
+    double **the_e_vectors;
+    double **the_T_mat;
+    double **miun_T_mat;
+    patt_jacobi = (double **)calc_jacob(len, lNormat);
+    if (NULL == patt_jacobi)
         return NULL;
-
-  
-    eigenvectors = jacobi_pattern + 1; 
-    get_mat_transe(eigenvectors, len);  
-    miun_T_mat = sort_matrix_values(jacobi_pattern, len);
-    if (miun_T_mat == NULL)
-        return NULL;
-
-
-    if (*K == 0)
-        eigengap_heuristic(miun_T_mat[0], len, K);
-
-    eigenvectors = miun_T_mat + 1; 
-    get_mat_transe(eigenvectors, len);
-    the_T_mat = set_T(eigenvectors, len, *K);
-    free_memory(miun_T_mat, len + 1);
+    the_e_vectors = 1 + patt_jacobi;
+    get_mat_transe(the_e_vectors, len);  
+    miun_T_mat = sortMatValues(patt_jacobi, len);
+    if(miun_T_mat == NULL || *K == 0){
+        if (miun_T_mat == NULL)
+             return NULL;
+        if (*K == 0) heru_eigen(miun_T_mat[0], len, K);
+    }
+    the_e_vectors = 1 + miun_T_mat; 
+    get_mat_transe(the_e_vectors, len);
+    the_T_mat = set_T(the_e_vectors, len, *K);
+    free_memory(miun_T_mat, 1 + len);
     return the_T_mat;
 }
 
@@ -28,15 +63,14 @@ void free_sort(double** mat){
     free(mat);
 }
 
-double **sort_matrix_values(double **mat, int dim){
+double **sortMatValues(double **mat, int dim){
     int i, j, max_of_index;
-    double the_max_value;
+    double the_max_value, temp;
     double **sort_mat = alloc_for_mat(dim + 1, dim);
-    if (sort_mat == NULL){
+    if (NULL == sort_mat){
         free_memory(mat, dim + 1);
         return NULL;
     }
-
     i  = 0;
     while(i < dim){
         max_of_index = -1, the_max_value = -1;
@@ -48,48 +82,12 @@ double **sort_matrix_values(double **mat, int dim){
             }
             j++;
         }
-        sort_mat[0][i] = the_max_value;
+        temp = the_max_value;
+        sort_mat[0][i] = temp;
         sort_mat[i + 1] = mat[max_of_index + 1];
         mat[0][max_of_index] = -1;
         i++;
     }
-   
     free_sort(mat);
     return sort_mat;
-}
-
-
-double **set_T(double **the_u_mat, int len, int K){
-    int i, j, in;
-    double sum = 0;
-    double **the_T_mat = alloc_for_mat(len, K);
-    if (the_T_mat == NULL)
-        return NULL;
-
-    for (i = 0; i < len; i++){
-        for (j = 0; j < K; j++){
-           
-            if (j == 0){
-                sum = 0;
-                for (in = 0; in < K; in++)
-                    sum += pow(the_u_mat[i][in], 2);
-            }
-            the_T_mat[i][j] = (sum != 0) ? (the_u_mat[i][j] / sqrt(sum)) : 0;
-        }
-    }
-    return the_T_mat;
-}
-
-
-void eigengap_heuristic(double *eigenvalues, int len, int *K){ 
-    int i;
-    double curr_max_gap = DBL_MIN;
-
-
-    for (i = 1; i <= (int)(len / 2); i++){
-        if (curr_max_gap < fabs(eigenvalues[i - 1] - eigenvalues[i])){
-            curr_max_gap = fabs(eigenvalues[i - 1] - eigenvalues[i]);
-            *K = i;
-        }
-    }
 }
