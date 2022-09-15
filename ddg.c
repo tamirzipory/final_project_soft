3613639bba2a10b03e5f17e4f1712606f07e775f
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-15 17:39:39 +0300

    Update ddg.c

--- ddg.c
+++ ddg.c
@@ -1,15 +1,16 @@
 double **diag_mat(double **mat, int len){
     int i, j;
     double sum;
-    double **diag = matrix_allocation(len, len);
+    double **diag = alloc_for_mat(len, len);
     if (diag == NULL)
         return NULL;
     sum = 0, i = 0;
     while(i < len){
-        sum = 0;
-        for (j = 0; j < len; j++){
+        sum = 0, j = 0;
+        while (j < len){
             sum = mat[i][j] + sum;
             diag[i][j] = 0;
+            j++;
         }
         diag[i][i] = sum;
         i++;
