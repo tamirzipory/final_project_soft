c5ab525907f4f8d9631e07cbe620d071da058185
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-15 17:05:21 +0300

    Update wam.c

--- wam.c
+++ wam.c
@@ -7,22 +7,19 @@ double **adjacency_matrix(double **mat_of_vectors, int dimension, int len){
 
     for (i = 0; i < len; i++){
         for (j = i; j < len; j++){
-            adj_mat[i][j] = (i == j) ? 0 : (exp((calc_euclidean_norm(mat_of_vectors[i], mat_of_vectors[j], dimension)) / (-2)));
+            adj_mat[i][j] = (i == j) ? 0 : (exp((euc_norm_calc(mat_of_vectors[i], mat_of_vectors[j], dimension)) / (-2)));
             adj_mat[j][i] = adj_mat[i][j];
         }
     }
     return adj_mat;
 }
 
-
-double calc_euclidean_norm(double *arr1, double *arr2, int dimension){
+double euc_norm_calc(double *arr1, double *arr2, int dimension){
     int j;
     double sum, ret;
     sum = 0;
     for (j = 0; j < dimension; j++)
-        sum = sum+((arr1[j]-arr2[j]) * (arr1[j]-arr2[j]));
-      /*  sum += pow(x[j] - y[j], 2);*/
-    
+        sum = sum+((arr1[j]-arr2[j]) * (arr1[j]-arr2[j]));    
     sum = sqrt(sum);
     ret = sum;
     return ret;
