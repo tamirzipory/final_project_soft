d979315adc061be4039dd034db4b77ee95e07456
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-15 17:45:31 +0300

    Update spkmeans.c

--- spkmeans.c
+++ spkmeans.c
@@ -1,101 +1,199 @@
-double **spk_algo(double **lnorm, int N, int *K){ /* Called after steps 1-2 have been made*/
-    double **jacobi_output, **eigenvectors, **T, **sort_transpose;
+#include "spkmeans.h"
+#include "spk.c"
+#include "wam.c"
+#include "ddg.c"
+#include "lnorm.c"
+#include "jacobi.c"
 
-    jacobi_output = (double **)calc_jacob(N, lnorm);
-    if (jacobi_output == NULL)
-        return NULL;
-
-    /* Transpose on eigenvectors- to make the sort easier*/
-    eigenvectors = jacobi_output + 1; /* Jacobi without eigenvalues*/
-    get_mat_transe(eigenvectors, N);
-
-    /* in sort_transpose jacobi_output is being freed, there is no use in it again!*/
-    sort_transpose = sort_matrix_values(jacobi_output, N);
-    if (sort_transpose == NULL)
-        return NULL;
-
-    /* The Eigengap Heuristic- was told not to handle a case where k=1*/
-    if (*K == 0)
-        eigengap_heuristic(sort_transpose[0], N, K);
-
-    /* Transpose on eigenvectors- get them to the right shape (vector in columns)*/
-    eigenvectors = sort_transpose + 1; /* sort_transpose without eigenvalues*/
-    get_mat_transe(eigenvectors, N);
-
-    /* eigenvectors points to the start of eigenvectors, we will use only the first K vectors (first K columns) as U
-     * and update eigenvectors (by renormalizing each of U’s rows) to be T */
-    T = set_T(eigenvectors, N, *K);
-    free_memory(sort_transpose, N + 1);
-
-    return T;
+void invalid_input(){
+    printf("Invalid Input!\n");
+    exit(1);
 }
 
-/* Receives a jacobi's matrix
- * Sort first row and rows 1 to N according to the eigenvalues in first row */
-double **sort_matrix_values(double **mat, int N){
-    int i, j, max_index;
-    double max_value;
-    double **sort_mat = alloc_for_mat(N + 1, N);
-    if (sort_mat == NULL){
-        free_memory(mat, N + 1);
-        return NULL;
-    }
-    for (i = 0; i < N; i++){
-        max_index = -1;
-        max_value = -1;
-        for (j = 0; j < N; j++){
-            /* Found new max*/
-            if (max_value < mat[0][j]){
-                max_index = j;
-                max_value = mat[0][j];
-            }
-        }
-        /* Place the i'th eigenvalue in the i'th cell and it's correspoond eigenvectors in line number i+1 */
-        sort_mat[0][i] = max_value;
-        sort_mat[i + 1] = mat[max_index + 1];
-        mat[0][max_index] = -1;
-    }
-    /* free (mat=jacobi_output) */
-    free(mat[0]);
-    free(mat);
-    return sort_mat;
+void err_print(){
+    printf("An Error Has Occurred\n");
+    exit(1);
 }
 
-/* Receives U (created by largest eigenvectors of jacobi), N- number of rows, K- number of columns
- * Returns T- by renormalizing each of U’s rows to have unit length */
-double **set_T(double **U, int N, int K){
-    int i, j, q;
-    double sum = 0;
-
-    double **T = alloc_for_mat(N, K);
-    if (T == NULL)
-        return NULL;
-
-    for (i = 0; i < N; i++){
-        for (j = 0; j < K; j++){
-            /* Calculate sum once for each new row!*/
-            if (j == 0){
-                sum = 0;
-                for (q = 0; q < K; q++)
-                    sum += pow(U[i][q], 2);
-            }
-            T[i][j] = (sum != 0) ? (U[i][j] / sqrt(sum)) : 0;
-        }
-    }
-    return T;
-}
-
-/* Receives eigenvalues (in decreasing order), N- number of values, K- number of clusters
- * Calculate K as described (Eigengap Heuristic algorithm) */
-void eigengap_heuristic(double *eigenvalues, int N, int *K){ /* (Assumption) lnorm formed as a decreasing ordered eigenvalues*/
+/*alloc mat according the dim*/
+double **alloc_for_mat(int rows, int cols){
     int i;
-    double curr_max_gap = DBL_MIN;
-
-    /* lmda(1)= E[0]>=lmda(2)=E[1]>=...>=lmda(n/2)=E[(N/2)-1]>=0*/
-    for (i = 1; i <= (int)(N / 2); i++){
-        if (curr_max_gap < fabs(eigenvalues[i - 1] - eigenvalues[i])){
-            curr_max_gap = fabs(eigenvalues[i - 1] - eigenvalues[i]);
-            *K = i;
+    double **mat = calloc(rows, (sizeof(double *)));
+    if (NULL == mat)
+        err_print();
+    i = 0;
+    while (i < rows){
+        mat[i] = calloc(cols, (sizeof(double)) );
+        if (NULL == mat[i])
+            err_print();
+        i++;
+    }
+    return mat;
+}
+/*return the dimentins according the purpose (dim or num of vectors*/
+int get_n_d_parameters(FILE *ifp, int situattion){
+    char ch;
+    int count = 0;
+    ch = 0;
+    while ((ch = fgetc(ifp)) != EOF){
+        if(situattion != 1){
+            if ('\n' == ch){
+                rewind(ifp);
+                return (1 + count);
+            }
+            else{
+                if (',' == ch)
+                    count = count + 1;
+                }
+        }
+        else{
+            if ('\n' == ch)
+                count = count + 1;
         }
     }
+    rewind(ifp);
+    return count;
+}
+
+/* receive the mats copy_mat,original_mat and dimentions of them and copy the value of copy_mat of the values of original_mat */
+void matrix_copy(int rows, int cols, double **copy_mat, double **original_mat){
+    int i, j;
+    for (i = 0; i < rows; i++){
+        j = 0;
+        while(j < cols){
+            copy_mat[i][j] = original_mat[i][j];
+            j++;
+        }        
+    }
+}
+
+/* set the inputs of the file to the mat of data according the dimentions*/
+void set_input(FILE *ifp, double **data, int rows, int cols){
+    int i, j;
+    double value;
+    i = 0, j = 0;
+    for (i = 0; i < rows; i++){
+        j = 0;
+        while(j < cols){
+            if(1 != fscanf(ifp, "%lf", &value))
+                  j--;
+            else data[i][j] = value;
+            fgetc(ifp);
+            j++;
+        }
+    }
+    rewind(ifp);
+}
+
+/* recive mat and free it*/
+void free_memory(double **mat, int rows){
+    int i = 0;
+    while(i < rows){
+        free(mat[i]);
+        i++;
+    }
+    free(mat);
+}
+
+/*Replace the */
+void msg_and_exit(int type_of_err, int err){
+    if (err == 1){
+        if (type_of_err != 0)
+            err_print();
+        else invalid_input();
+    }
+}
+
+/* print matrix according the pourpose that we got from the user*/
+void print_result(double **mat, int rows, int cols, enum Goal target)
+{
+    int i, j;
+    if (target == 4)
+        rows++;
+    i = 0;
+    while(i < rows){
+        j = 0;
+        while(j < cols){
+            if (cols - 1 == j)
+                printf("%.4f", mat[i][j]);
+            else printf("%.4f,", mat[i][j]);
+            j++;
+        }
+        if ((rows - 1) != i)
+            printf("\n");
+        i++;
+    }
+}
+
+/*the running function */
+double **run_goal(enum Goal target, double **data, int n1, int n2, int *n3){
+    double **ret;
+    double **mat_dd,**mat_wam, **mat_lnorm;
+    if (target == 4)
+        return calc_jacob(n1, data);
+    
+    ret = adjacency_matrix(data, n2, n1);
+    if (ret == NULL)
+        return NULL;
+    if(target == 1)
+        return ret;
+    mat_wam = ret;
+    ret = diag_mat(mat_wam, n1);
+    if (target == 2 ||ret == NULL){
+        free_memory(mat_wam, n1);
+        return ret;
+    }
+    mat_dd = ret;
+    ret = calc_L_mat(mat_dd, mat_wam, n1);
+    free_memory(mat_wam, n1);
+    free_memory(mat_dd, n1);
+
+    if (target == 3 || ret == NULL)
+        return ret;
+    mat_lnorm = ret;
+    ret = spk_algo(mat_lnorm, n1, n3);
+    free_memory(mat_lnorm, n1);
+    return ret;
+}
+
+/*The main function */
+int main(int argc, char *argv[]){
+    double **data, **ret;
+    int n1, n2, n3;
+    FILE *ifp;
+    enum Goal target = 0;
+    n3 = 0;
+    msg_and_exit(0, argc != 3);
+
+    if (strcmp("wam", argv[1]) == 0)
+        target = wam_g;
+    else if (strcmp("ddg", argv[1]) == 0)
+        target = ddg_g;
+    else if (strcmp("lnorm", argv[1]) == 0)
+        target = lnorm_g;
+    else if (strcmp("jacobi", argv[1]) == 0)
+        target = jacobi_g;
+    msg_and_exit(0, 0 == target);
+
+    ifp = fopen(argv[2], "r");
+    msg_and_exit(1, ifp == NULL);
+    n1 = get_n_d_parameters(ifp, 1);
+    n2 = get_n_d_parameters(ifp, 2);
+    data = alloc_for_mat(n1, n2);
+    msg_and_exit(1, data == NULL);
+    set_input(ifp, data, n1, n2);
+    ret = run_goal(target, data, n1, n2, &n3);
+    if (NULL == ret){ 
+        free_memory(data, n1);
+        msg_and_exit(1, 1);
+    }
+
+    print_result(ret, n1, n1, target);
+    printf("\n");
+    free_memory(data, n1);
+    if (jacobi_g != target)
+        free_memory(ret, n1);
+    else free_memory(ret, n1 + 1);
+    fclose(ifp);
+    exit(0);
 }
