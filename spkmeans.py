08ec45592f9708e1355e7bee732f31d780e52312
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-15 10:49:39 +0300

    Update spkmeans.py

--- spkmeans.py
+++ spkmeans.py
@@ -11,24 +11,24 @@ class Goal(Enum):
     lnorm = 3
     jacobi = 4
     spk = 5
-    spk_ex2 = 6
+    second_kmeans = 6
 
 
-def print_output(mat_from_fit, N, goal):
-    output_res = ""
-    num_rows = N
+def print_output(mat_from_fit, len_of, goal):
+    str_ret = ""
+    num_rows = len_of
 
     if(goal == Goal.jacobi):
         num_rows += 1
 
     for i in range(num_rows):
-        for j in range(N):
-            output_res += str('%.4f' % (mat_from_fit[i][j]))
-            if (j != N-1):
-                output_res += ","
+        for j in range(len_of):
+            str_ret += str('%.4f' % (mat_from_fit[i][j]))
+            if (j != len_of-1):
+                str_ret += ","
         if (i != num_rows-1):
-            output_res += "\n"
-    print(output_res)
+            str_ret += "\n"
+    print(str_ret)
 
 def handle_errors():
     print("An Error Has Occurred")
@@ -54,30 +54,31 @@ def get_goal_input(filename):
         handle_errors()
 
 
-def print_output_spk(mat_from_fit, K, D, centroids_index_list):
-    output_res = ""
+def print_output_spk(mat_from_fit, K, arr2, list_cent_index):
+    str_ret = ""
     for i in range(K):
-        output_res += str(int(centroids_index_list[i]))
+        str_ret += str(int(list_cent_index[i]))
         if (i != K-1):
-            output_res += ","
-    output_res += "\n"
+            str_ret += ","
+    str_ret += "\n"
 
     
     for i in range(K):
-        for j in range(D):
-            output_res += str('%.4f' % (mat_from_fit[i][j]))
-            if (j != D-1):
-                output_res += ","
+        for j in range(arr2):
+            str_ret += str('%.4f' % (mat_from_fit[i][j]))
+            if (j != arr2-1):
+                str_ret += ","
         if ( i != K-1):
-            output_res += "\n"
-    print(output_res)
+            str_ret += "\n"
+    print(str_ret)
 
 
-def check_input(given_input, argLen):
-    if (argLen != 4):
+def check_input(given_input, argc):
+    if (argc != 4):
         handle_errors_input()
    
-    is_valid = given_input[1].isnumeric()
+    is_valid = given_input[1]
+    is_valid = is_valid.isnumeric()
     if is_valid:
         is_valid = int(given_input[1]) >= 0 
     
@@ -90,44 +91,45 @@ def check_input(given_input, argLen):
     return ((int)(given_input[1]), Goal[given_input[2]])
 
 
-def kMeans_init(K, data_points_array):
+def start_kmeans(K, data_points_array):
 
     Centroids_array = []  
     Centroids_index_array = []
 
-    N = len(data_points_array)
-    D_array = np.array([0.0 for i in range(N+1)])
-    Pr_array = np.array([0.0 for i in range(N)])
-    Index_array = np.array([i for i in range(N)])
+    len_of = len(data_points_array)
+    arr_of_distance = np.array([0.0 for i in range(len_of+1)])
+    arr_of_vectors = np.array([0.0 for i in range(len_of)])
+    arr_of_index = np.array([i for i in range(len_of)])
 
     np.random.seed(0)
 
-    index=np.random.choice(Index_array)
+    index=np.random.choice(arr_of_index)
     Centroids_index_array.append(index)
     Centroids_array.append(data_points_array[index]) 
 
     for i in range(1, K):  
-        find_D(D_array, data_points_array, N, Centroids_array)  
+        find_D(arr_of_distance, data_points_array, len_of, Centroids_array)  
         
-        Pr_array = np.array([(D_array[l] / D_array[N]) for l in range(N)])
-        index = np.random.choice(Index_array, p=Pr_array)
+        arr_of_vectors = np.array([(arr_of_distance[l] / arr_of_distance[N]) for l in range(N)])
+        index = np.random.choice(arr_of_index, p=arr_of_vectors)
         Centroids_index_array.append(index)
         Centroids_array.append(data_points_array[index])
     return Centroids_index_array,Centroids_array
 
 
-def find_D(D_array, datapoints_array, N, Centroids_array):
-    D_array[N] = 0.0
-    for l in range(N):
+def find_D(arr_of_distance, datapoints_array, len_of, Centroids_array):
+    arr_of_distance[len_of] = 0.0
+    for l in range(len_of):
        
-        D_array[l] = np.min([calc(datapoints_array[l], centroid) for centroid in Centroids_array])
+        arr_of_distance[l] = np.min([calc(datapoints_array[l], centroid) for centroid in Centroids_array])
         
-        D_array[N] = D_array[N] + D_array[l]
+        arr_of_distance[len_of] = arr_of_distance[len_of] + arr_of_distance[l]
 
 
-def calc(x, y):  
-    z = np.subtract(np.array(x), np.array(y))
-    return np.sum(np.multiply(z, z))
+def calc(arr1, arr2):  
+    sub = np.subtract(np.array(arr1), np.array(arr2))
+    ret = np.sum(np.multiply(sub, sub))
+    return ret
 
 
 def call_fit_ex2(N, K, dimension, data_points, centroids, goal):
@@ -145,21 +147,21 @@ def main(argv):
     inputs = argv
     inputs_len = len(inputs)
     K, goal = check_input(argv, inputs_len)
-    data_points_array, N, D = get_goal_input(argv[3])
+    data_points_array, len, the_g = get_goal_input(argv[3])
 
-    if K > N:
+    if K > len:
         handle_errors_input()
     try:
-        goal_matrix = spkmeans_module.fit(N, K, D, data_points_array.tolist(), goal.value, [])
+        goal_matrix = spkmeans_module.fit(len, K, the_g, data_points_array.tolist(), goal.value, [])
         if(goal != Goal.spk):
-            print_output(goal_matrix, N, goal)
+            print_output(goal_matrix, len, goal)
         else:
             if(K == 0):
                 K = len(goal_matrix[0])
-            goal=Goal.spk_ex2
-            centroids_index, centroids = kMeans_init(K, goal_matrix)
-            D=K
-            print_output_spk(call_fit_ex2(N, K, D,goal_matrix, centroids, goal), K, D, centroids_index)
+            goal=Goal.second_kmeans
+            centroids_index, centroids = start_kmeans(K, goal_matrix)
+            the_g=K
+            print_output_spk(call_fit_ex2(len, K, the_g,goal_matrix, centroids, goal), K, the_g, centroids_index)
 
     except:
         handle_errors()
