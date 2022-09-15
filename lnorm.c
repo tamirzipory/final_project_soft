6e77ead0c0994e5615903adb165a7e89c89796b1
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-15 18:08:15 +0300

    Update lnorm.c

--- lnorm.c
+++ lnorm.c
@@ -1,54 +1,49 @@
-double **calc_L_mat(double **diag_mat, double **adj_mat, int len){
+double **calc_L_mat(double **d_mat, double **a_mat, int len){
    
-    double **mul1, **mul2, **ret;
-
-    cal_D12(diag_mat, len);
-    mul1 = calc_mul(len, diag_mat, adj_mat);
-    if (mul1 == NULL)
+    double **m1;
+    double **m2;
+    double **ret;
+    cal_D12(d_mat, len);
+    m1 = calc_mul(len, d_mat, a_mat);
+    if (m1 == NULL)
         return NULL;
-
-    mul2 = calc_mul(len, mul1, diag_mat);
-    free_memory(mul1, len);
-    if (mul2 == NULL)
+    m2 = calc_mul(len, m1, d_mat);
+    if (m1 == NULL)
         return NULL;
-
+    free_memory(m1, len);
+    if (m2 == NULL)
+        return NULL;
     ret = calc_id_mat(len);
+    if (m2 == NULL)
+        return NULL;
     if (ret == NULL){
-        free_memory(mul2, len);
+        free_memory(m2, len);
         return NULL;
     }
-    sab_matrix(len, ret, mul2);
-    free_memory(mul2, len);
+    sab_matrix(len, ret, m2);
+    free_memory(m2, len);
     return ret;
 }
-
 
 double divide_lnorm(double** mat, int i, int j){
     return (1 / sqrt(mat[i][j]));
 }
 
-/* Receives D- diagonal degree matrix, N- number of rows/columns
- * Updates D to D^(-0.5)*/
-void cal_D12(double **diag_mat, int len){
-    int i;
-    for (i = 0; i < len; i++){
-        diag_mat[i][i] = divide_lnorm(diag_mat, i, i);
-        /*
-        diag_mat[i][i] = 1 / sqrt(diag_mat[i][i]);
-        */
+void cal_D12(double **d_mat, int len){
+    int i=0;
+    while (i < len)
+    {
+        d_mat[i][i] = divide_lnorm(d_mat, i, i);
+        i++;
     }
 }
 
-/* Receives matrices: A,B and N- number of rows/columns
- * Returns matrix C= A*B
- * If an error occurred returns NULL*/
 double **calc_mul(int dim_of_the_mats, double **mat1, double **mat2){
     int i, j, z;
     double temp;
     double **ret = alloc_for_mat(dim_of_the_mats, dim_of_the_mats);
     if (ret == NULL)
         return NULL;
-
     i = 0;
     while(dim_of_the_mats>i){
         j = 0;
@@ -65,21 +60,9 @@ double **calc_mul(int dim_of_the_mats, double **mat1, double **mat2){
         i++;
     }
 
-/*
-    for (i = 0; i < dim_of_the_mats; i++)
-    {
-        for (j = 0; j < dim_of_the_mats; j++){
-            ret[i][j] = 0;
-            for (z = 0; z < dim_of_the_mats; k++)
-                ret[i][j] += mat1[i][z] * mat2[z][j];   
-        }
-    }
-    */
     return ret;
 }
 
-/* Receives matrices: A,B and N- number of rows/columns
- * Updates A= A-B */
 void sab_matrix(int dim, double **mat1, double **mat2){
     int i, j;
     double temp;
@@ -93,46 +76,24 @@ void sab_matrix(int dim, double **mat1, double **mat2){
         }
         i++;
     }
-    /*
-    for (i = 0; i < dim; i++){
-        for (j = 0; j < dim; j++)
-            mat1[i][j] -= mat2[i][j];
-    }
-    */
 }
 
 /*calculate id-mat*/
 double **calc_id_mat(int dim_of_mat){
     int i, j;
     double **id_mat = alloc_for_mat(dim_of_mat, dim_of_mat);
-    if (id_mat == NULL)
+    if (NULL == id_mat)
         return NULL;
-
     i = 0, j = 0;
     while(i < dim_of_mat){
         j = 0;
         while(dim_of_mat > j){
-            if(i == j){
+            if(i == j)
                 id_mat[i][j] = 1;
-            }
-            else{
-                id_mat[i][j] = 0;
-            }
+            else id_mat[i][j] = 0;
             j = j + 1;;
         }
         i = i+1;
     }
-/*
-    for (i = 0; i < dim_of_mat; i++){
-        for (j = 0; j < dim_of_mat; j++)
-            if(i == j){
-                id_mat[i][j] = 1;
-            }
-            else{
-                id_mat[i][j] = 0;
-            }
-
-    }
-    */
     return id_mat;
 }
