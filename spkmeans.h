29f2dac683f9897ca45b72e969d40b4dd328fd7a
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-15 21:11:02 +0300

    Update spkmeans.h

--- spkmeans.h
+++ spkmeans.h
@@ -32,28 +32,26 @@ double **calc_spk_method(double **, int , int *);
 double **mat_sorting(double **, int );
 void func_heruicsic(double *, int , int *);
 double **calc_the_T(double **, int , int );
-
-/* (spkmeans.c) (C) main's functions*/
-double **run_goal(enum Goal goal, double **data_input, int len, int D, int *K);
-void print_result(double **mat, int num_rows, int num_cols, enum Goal goal);
-void msg_and_exit(int error_type, int is_error);
-int get_n_d_parameters(FILE *ifp, int situation);
-void set_input(FILE *ifp, double **data_input, int num_of_rows, int num_of_cols);
-void free_memory(double **ArrayToFree, int num_rows);
-double **calc_jacob(int len, double **A);
-void matrix_copy(int num_rows, int num_cols, double **dest_mat, double **src_mat);
-void A_to_A_tag(int len, double **A, int *i_pointer, int *j_pointer);
-void get_params(double **A, int i, int j, double *point_1, double *point_2);
-void calc_curr_P(int len, double **P_mat, int i, int j, double d1, double d2);
-void get_values_from_first_mat(double *values, int len, double **mat);
-void get_mat_transe(double **mat, int N);
-double **miun_of_eig(int len, double *values, double **vectors);
-void calc_first_mat(int len, double **mat, double d1, double d2, int i, int j, int *return_value);
-double calc_off_diag(int len, double **A);
+double **run_goal(enum Goal , double **, int , int , int *);
+void print_result(double **, int , int , enum Goal );
+void msg_and_exit(int , int );
+int get_n_d_parameters(FILE *, int );
+void set_input(FILE *, double **, int , int );
+void free_memory(double **, int );
+double **calc_jacob(int , double **);
+void matrix_copy(int , int , double **, double **);
+void A_to_A_tag(int , double **, int *, int *);
+void get_params(double **, int , int , double *, double *);
+void calc_curr_P(int , double **, int , int , double , double );
+void get_values_from_first_mat(double *, int , double **);
+void get_mat_transe(double **, int );
+double **miun_of_eig(int , double *, double **);
+void calc_first_mat(int , double **, double , double , int , int , int *);
+double calc_off_diag(int , double **);
 
 
-int kMeans(int len, int K, double **points, double **cent, int dimension);
-int checkTheNorm(double **fresh, double **old, int dimension, int K);
-int assign_cluster(double **cent, double *Datapoint, int dimension, int K);
-void idcon_cent(double **fresh, double **old, int dimension, int K);
+int kMeans(int , int , double **, double **, int );
+int checkTheNorm(double **, double **, int , int );
+int assign_cluster(double **, double *, int , int );
+void idcon_cent(double **, double **, int , int );
 #endif
