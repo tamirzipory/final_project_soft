de16e37eeb4000791e23e67e003d60886b970f46
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-15 19:29:25 +0300

    Update wam.c

--- wam.c
+++ wam.c
@@ -7,21 +7,20 @@ double **adjacency_matrix(double **data_points, int dimension, int N){
 
     for (i = 0; i < N; i++){
         for (j = i; j < N; j++){
-            adj_mat[i][j] = (i == j) ? 0 : (exp((calc_euclidean_norm(data_points[i], data_points[j], dimension)) / (-2)));
+            adj_mat[i][j] = (i == j) ? 0 : (exp((euc_norm_calc(data_points[i], data_points[j], dimension)) / (-2)));
             adj_mat[j][i] = adj_mat[i][j];
         }
     }
     return adj_mat;
 }
 
-/* Receives 2 vectors- x,y and thier dimension
- * Returns their distance: ||x-y||2*/
-double calc_euclidean_norm(double *x, double *y, int dimension){
+
+double euc_norm_calc(double *arr1, double *arr2, int dimension){
     int j;
     double sum;
     sum = 0;
     for (j = 0; j < dimension; j++)
-        sum += pow(x[j] - y[j], 2);
+        sum += pow(arr1[j] - arr2[j], 2);
     
     sum = sqrt(sum);
     return sum;
