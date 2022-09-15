c5790a70a29567aba720717ff6b7be7080e7b2f0
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-15 10:50:25 +0300

    Update wam.c

--- wam.c
+++ wam.c
@@ -1,28 +1,29 @@
-double **adjacency_matrix(double **data_points, int dimension, int N){
+double **adjacency_matrix(double **mat_of_vectors, int dimension, int len){
     int i, j;
 
-    double **adj_mat = matrix_allocation(N, N);
+    double **adj_mat = alloc_for_mat(len, len);
     if (adj_mat == NULL)
         return NULL;
 
-    for (i = 0; i < N; i++){
-        for (j = i; j < N; j++){
-            adj_mat[i][j] = (i == j) ? 0 : (exp((calc_euclidean_norm(data_points[i], data_points[j], dimension)) / (-2)));
+    for (i = 0; i < len; i++){
+        for (j = i; j < len; j++){
+            adj_mat[i][j] = (i == j) ? 0 : (exp((calc_euclidean_norm(mat_of_vectors[i], mat_of_vectors[j], dimension)) / (-2)));
             adj_mat[j][i] = adj_mat[i][j];
         }
     }
     return adj_mat;
 }
 
-/* Receives 2 vectors- x,y and thier dimension
- * Returns their distance: ||x-y||2*/
-double calc_euclidean_norm(double *x, double *y, int dimension){
+
+double calc_euclidean_norm(double *arr1, double *arr2, int dimension){
     int j;
-    double sum;
+    double sum, ret;
     sum = 0;
     for (j = 0; j < dimension; j++)
-        sum += pow(x[j] - y[j], 2);
+        sum = sum+((arr1[j]-arr2[j]) * (arr1[j]-arr2[j]));
+      /*  sum += pow(x[j] - y[j], 2);*/
     
     sum = sqrt(sum);
-    return sum;
+    ret = sum;
+    return ret;
 }
