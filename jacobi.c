22deb5d857cdb2f698628aaf6041e3a4292a3c5c
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-09-15 18:53:09 +0300

    Update jacobi.c

--- jacobi.c
+++ jacobi.c
@@ -1,31 +1,59 @@
-void calc_curr_P(int max_iter, double **the_p_mat, int i, int j, double d1, double d2){
-    int in1, in2;
-    in1 = 0;
-    while(in1 < max_iter){
-        in2 = 0;
-        while(in2 < max_iter){
-            if(in1 == in2){
-                if(in1 == i || in2 == j)
-                   the_p_mat[in1][in2] = d1;
-                else the_p_mat[in1][in2] = 1;   
-            }
-            else if (in1 == j && in2 == i)
-                the_p_mat[in1][in2] = -d2;
-            else{
-                if(in1 == i && in2 == j)
-                    the_p_mat[in1][in2] = d2;
-                else the_p_mat[in1][in2] = 0;
-            }
-            in2++;
-        }
-        in1++;
+double **calc_jacob(int len_mat, double **A){
+    int count, i, j, ret;
+    double d1, d2, first_A_mat, second_A_mat; 
+    double **V_mat, **the_p_mat, **mat_of_jacobi, **finish_V_mat;
+    double *values; 
+    V_mat = calc_id_mat(len_mat);
+    if (V_mat == NULL)
+        return NULL;
+
+    the_p_mat = matrix_allocation(len_mat, len_mat);
+    if (the_p_mat == NULL){
+        free_memory(V_mat, len_mat);
+        return NULL;
     }
+
+    values = calloc(len_mat, sizeof(double)); 
+    if (values == NULL){
+        free_memory(V_mat, len_mat);
+        free_memory(the_p_mat, len_mat);
+        return NULL;
+    }
+    count = 1, ret=1, first_A_mat = 1.00001, second_A_mat = 0;
+    for(;((first_A_mat - second_A_mat > 0.00001) || (count == 0)) && (100 > count); count++){
+        first_A_mat = calc_off_diag(len_mat, A);
+        A_to_A_tag(len_mat, A, &i, &j);                   
+        if (A[i][j] == 0)
+            break;
+        get_params(A, i, j, &d1, &d2);            
+        calc_curr_P(len_mat, the_p_mat, i, j, d1, d2);      
+        calc_first_mat(len_mat, A, d1, d2, i, j, &ret); 
+        finish_V_mat=V_mat, V_mat = calc_mul(len_mat, V_mat, the_p_mat); 
+        free_memory(finish_V_mat,len_mat);
+        if(ret == 0){
+            free(values);
+            free_memory(the_p_mat, len_mat);
+            return NULL;
+        }
+        if (V_mat == NULL){ 
+            free(values);
+            free_memory(the_p_mat, len_mat);
+            return NULL;
+        }
+        second_A_mat = calc_off_diag(len_mat, A);
+    }
+    get_values_from_first_mat(values, len_mat, A);           
+    mat_of_jacobi = miun_of_eig(len_mat, values, V_mat); 
+    free_memory(V_mat, len_mat);
+    free_memory(the_p_mat, len_mat);
+    free(values);
+    return mat_of_jacobi;
 }
 
 
 void calc_first_mat(int len_mat, double **A, double d1, double d2, int i, int j, int *ret){
     int r;
-    double **rows_cols = alloc_for_mat(2, len_mat);
+    double **rows_cols = matrix_allocation(2, len_mat);
     if (NULL == rows_cols){
         *ret = 0;
         return;
@@ -129,15 +157,6 @@ double calc_s(double t){
     return t / sqrt(1 + (t * t));
 }
 
-void get_values_from_first_mat(double *values, int len, double **mat){
-    int i = 0;
-    while( i < len){
-        values[i] = mat[i][i];
-        i++;
-    }
-}
-
-
 void get_params(double **A, int i, int j, double *p1, double *p2){
     double th, t;
     double sign = 1;
@@ -153,66 +172,44 @@ void get_params(double **A, int i, int j, double *p1, double *p2){
     *p1 = divide(t);
     *p2 = t / sqrt((t * t) + 1);
 }
-
-double **calc_jacob(int len_mat, double **A){
-    int count, i, j, ret;
-    double d1, d2, first_A_mat, second_A_mat; 
-    double **V_mat, **the_p_mat, **mat_of_jacobi, **finish_V_mat;
-    double *values; 
-    V_mat = calc_id_mat(len_mat);
-    if (V_mat == NULL)
-        return NULL;
-
-    the_p_mat = alloc_for_mat(len_mat, len_mat);
-    if (the_p_mat == NULL){
-        free_memory(V_mat, len_mat);
-        return NULL;
-    }
-
-    values = calloc(len_mat, sizeof(double)); 
-    if (values == NULL){
-        free_memory(V_mat, len_mat);
-        free_memory(the_p_mat, len_mat);
-        return NULL;
-    }
-    count = 1, ret=1, first_A_mat = 1.00001, second_A_mat = 0;
-    for(;((first_A_mat - second_A_mat > 0.00001) || (count == 0)) && (100 > count); count++){
-        first_A_mat = calc_off_diag(len_mat, A);
-        A_to_A_tag(len_mat, A, &i, &j);                   
-        if (A[i][j] == 0)
-            break;
-        get_params(A, i, j, &d1, &d2);            
-        calc_curr_P(len_mat, the_p_mat, i, j, d1, d2);      
-        calc_first_mat(len_mat, A, d1, d2, i, j, &ret); 
-        finish_V_mat=V_mat, V_mat = calc_mul(len_mat, V_mat, the_p_mat); 
-        free_memory(finish_V_mat,len_mat);
-        if(ret == 0){
-            free(values);
-            free_memory(the_p_mat, len_mat);
-            return NULL;
+void calc_curr_P(int max_iter, double **the_p_mat, int i, int j, double d1, double d2){
+    int in1, in2;
+    in1 = 0;
+    while(in1 < max_iter){
+        in2 = 0;
+        while(in2 < max_iter){
+            if(in1 == in2){
+                if(in1 == i || in2 == j)
+                   the_p_mat[in1][in2] = d1;
+                else the_p_mat[in1][in2] = 1;   
+            }
+            else if (in1 == j && in2 == i)
+                the_p_mat[in1][in2] = -d2;
+            else{
+                if(in1 == i && in2 == j)
+                    the_p_mat[in1][in2] = d2;
+                else the_p_mat[in1][in2] = 0;
+            }
+            in2++;
         }
-        if (V_mat == NULL){ 
-            free(values);
-            free_memory(the_p_mat, len_mat);
-            return NULL;
-        }
-        second_A_mat = calc_off_diag(len_mat, A);
+        in1++;
     }
-    get_values_from_first_mat(values, len_mat, A);           
-    mat_of_jacobi = miun_of_eig(len_mat, values, V_mat); 
-    free_memory(V_mat, len_mat);
-    free_memory(the_p_mat, len_mat);
-    free(values);
-    return mat_of_jacobi;
 }
 
+void get_values_from_first_mat(double *values, int len, double **mat){
+    int i = 0;
+    while( i < len){
+        values[i] = mat[i][i];
+        i++;
+    }
+}
 
 /*It's do merge of the eighens*/
 double **miun_of_eig(int len, double *values, double **vectors){
     double **ret = NULL;
     int i;
     int plus_one = len+1;
-    ret = alloc_for_mat(plus_one, len);
+    ret = matrix_allocation(plus_one, len);
     if (ret == NULL)
         return NULL;
     i = 0;
